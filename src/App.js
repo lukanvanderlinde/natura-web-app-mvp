809722e285fb0a5c0fd964a71e14a56e60a8abf5
Lukan Vanderlinde <lukanvanderlinde@gmail.com>
2020-03-21 12:33:38 -0300

    Primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,15 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <iframe
+        title='site'
+        src='https://www.somosbem.com.br/tablet'
+        height='800px'
+        width='1280px'></iframe>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
